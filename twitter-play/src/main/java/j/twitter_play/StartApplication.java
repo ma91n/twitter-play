6b7032a45abbec678d424cf380182e9c3cf23a64
mano-junki <mano.junki@gmail.com>
2012-12-02 19:23:27 +0900

    よけいなimportを除外

--- twitter-play/src/main/java/j/twitter_play/StartApplication.java
+++ twitter-play/src/main/java/j/twitter_play/StartApplication.java
@@ -1,15 +1,9 @@
 package j.twitter_play;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-
 import twitter4j.Status;
 import twitter4j.Twitter;
-import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 import twitter4j.auth.AccessToken;
-import twitter4j.auth.RequestToken;
-import twitter4j.conf.ConfigurationBuilder;
 
 /**
  * sample-executor
